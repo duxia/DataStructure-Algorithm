e2d0098821c8703a1a2a18d5c459dbc4744754a2
coffee <226639265@qq.com>
2015-03-20 15:32:19 +0800

    fix a bug

--- 算法练习/JulyCodingExercise/LongestPalindromeSubStr.cpp
+++ 算法练习/JulyCodingExercise/LongestPalindromeSubStr.cpp
@@ -19,7 +19,7 @@ int LongestPalindromeSubStr(string &str)
     int tmp=0,max=0;
     for(int i=0;i<length;i++) {//i为中心元素下标
         //str[i-1]=str[i+1],奇数情况
-        for(int offset=0; i-offset>0 && i+offset<length ;offset++) {//offset为相对于i的偏移量
+        for(int offset=0; i-offset>=0 && i+offset<length ;offset++) {//offset为相对于i的偏移量
             if(str[i-offset] != str[i+offset])
                 break;
             tmp = offset*2 + 1;
@@ -27,7 +27,7 @@ int LongestPalindromeSubStr(string &str)
         if(tmp > max)
             max = tmp;
         //str[i]=str[i+1],偶数情况
-        for(int offset=0; i-offset>0 && i+offset+1<length ;offset++) {
+        for(int offset=0; i-offset>=0 && i+offset+1<length ;offset++) {
             if(str[i-offset] != str[i+offset+1])
                 break;
             tmp = offset*2 + 2;
